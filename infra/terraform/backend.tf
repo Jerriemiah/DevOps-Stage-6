b2229154234d77a78725dd2f80a99236af7b9451
Jerriemiah <inyiamajeremiah@gmail.com>
2025-11-29 10:44:10 +0100

    corrected depricated backend.tf format

--- infra/terraform/backend.tf
+++ infra/terraform/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "hng-stage6-terraform-state"
-    key            = "devops/terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "hng-terraform-locks"
-    encrypt        = true
+    bucket       = "hng-stage6-terraform-state"
+    key          = "devops/terraform.tfstate"
+    region       = "us-east-1"
+    use_lockfile = true
+    encrypt      = true
   }
 }
