101a7fab3d54d679500331f9d409ff0c26412923
Jerriemiah <inyiamajeremiah@gmail.com>
2025-11-30 10:15:31 +0100

    trigger drift test

--- frontend/config/prod.env.js
+++ frontend/config/prod.env.js
@@ -1,3 +1,6 @@
+'use strict'
 module.exports = {
-  NODE_ENV: '"production"'
+  NODE_ENV: '"production"',
+  AUTH_API_ADDRESS: '"https://auth.jerrie-todo.mooo.com"',
+  TODOS_API_ADDRESS: '"https://todos.jerrie-todo.mooo.com"'
 }
